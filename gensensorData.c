fe45f04677f061866c87fbbb761b2a1c38a0b30c
Ivann <ivann.galic@edu.itescia.fr>
2020-04-22 14:29:26 +0200

    Story 3 débutée

--- gensensorData.c
+++ gensensorData.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    printf("Bienvenue !\n");
+    
+    char standardEntry[20];
+    fgets(standardEntry,20,stdin);
+    printf("vous avez tapé, %s\n",standardEntry);
+
+    return(0);
 }    
