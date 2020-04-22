cbd23025784f9645a206b859dee2cf1e00f7e962
kevin <r_kevin@hotmail.fr>
2020-04-22 16:11:50 +0200

    Story 4 terminée

--- genSensorData.c
+++ genSensorData.c
@@ -8,10 +8,10 @@ int main(int argc, char *argv[]) {
     char gentickStatus[30];
     while (fgets(gentickStatus,30,stdin)) {
         if (strcmp(gentickStatus,"OK\n") == 0) {
-            printf("It's %s\n",gentickStatus);
+            printf("sensor;0;100;20\n");
         }
         else if (strcmp(gentickStatus,"ERROR\n") == 0) {
-            printf("There is an %s\n",gentickStatus);
+            printf("error;Impossible de lire la valeur\n");
         }
         
           
