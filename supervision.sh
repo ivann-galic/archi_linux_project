a81c3f8cc5c26f95caa795536ef95132ac19f052
kevin <r_kevin@hotmail.fr>
2020-04-23 16:11:45 +0200

    story 9 en cours

--- supervision.sh
+++ supervision.sh
@@ -6,6 +6,9 @@ infos_log_file=$3
 errors_log_file=$4
 file_size=$5
 
+
+
+
  # get the size of the logs files:
  if [ -e /home/$USER/$folder_name/$infos_log_file ] && [ -e /home/$USER/$folder_name/$errors_log_file ];
     then
@@ -16,6 +19,8 @@ file_size=$5
         if [ $size_infos -gt $file_size ];
         then
             echo "The size of $infos_log_file ($size_infos ko) is higher than $file_size Ko";
+            killall -9 generation.sh;
+            tar -zcvf "$(date '+%Y-%m-%d').tar.gz" $infos_log_file $errors_log_file;
         elif [ $size_infos -eq $file_size ];
         then
             echo "The size of $infos_log_file ($size_infos ko) is equal to $file_size Ko";
@@ -26,6 +31,8 @@ file_size=$5
         if [ $size_errors -gt $file_size ];
         then
             echo "The size of $errors_log_file ($size_errors ko) is higher than $file_size Ko";
+            killall -9 generation.sh;
+            tar -zcvf "$(date '+%Y-%m-%d').tar.gz" $infos_log_file $errors_log_file;
         elif [ $size_errors -eq $file_size ];
         then
             echo "The size of $errors_log_file ($size_errors ko) is equal to $file_size Ko";
