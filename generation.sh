2d44567c4c166647a8d9725d400ade49b85a0fa9
Ivann <ivann.galic@edu.itescia.fr>
2020-04-23 12:39:19 +0200

    Story 7 terminée

--- generation.sh
+++ generation.sh
@@ -5,10 +5,11 @@ folder_name=$2
 infos_log_file=$3
 errors_log_file=$4
 
-mkdir -p $folder_name
-touch $folder_name/$infos_log_file
-touch $folder_name/$errors_log_file
-myvariable=$USER
+myUserPath=/home/$USER
+mkdir -p $myUserPath/$folder_name
+touch $myUserPath/$folder_name/$infos_log_file
+touch $myUserPath/$folder_name/$errors_log_file
+
 
 # gets all the results form genTick and redirect it on a specific log file :
 ./genTick $delay | ./genSensorData 2>&1 | {
@@ -17,10 +18,11 @@ while IFS= read -r RAW_LINE; do
     result_to_write=$(echo $RAW_LINE | cut -d';' -f1,2,3,5,6);
     if [[ $line_cut == "sensor" ]];
     then
-        echo $result_to_write >> $folder_name/$infos_log_file;
+        echo $myUserPath/$folder_name/$infos_log_file
+        echo $result_to_write >> $myUserPath/$folder_name/$infos_log_file;
     elif [[ $line_cut == "error" ]];
     then
-        echo $RAW_LINE >> $folder_name/$errors_log_file;
+        echo $RAW_LINE >> $myUserPath/$folder_name/$errors_log_file;
     elif [[ $RAW_LINE == "Bienvenue !" ]];
     then
         continue;
