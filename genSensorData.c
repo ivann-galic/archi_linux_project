bfa21d36175fb9e560997e2aa40f9fa7354c6439
Ivann <ivann.galic@edu.itescia.fr>
2020-04-24 14:05:39 +0200

    story 10 terminée

--- genSensorData.c
+++ genSensorData.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+
 
 int main(int argc, char *argv[]) {
-    
+
     printf("Welcome !\n");
 
     char gentick_status[30];
     // gets the result frem genTick  and displays a message based on this status :
     while (fgets(gentick_status,30,stdin)) {
         if (strcmp(gentick_status,"OK\n") == 0) {
-            printf("sensor;1668457613;garage;0.5;2;2.5\n");
+            int myRandom=rand()%100000;
+            printf("sensor;%d;garage;0.5;2;2.5\n",myRandom);
         }
         else if (strcmp(gentick_status,"ERROR\n") == 0) {
-            printf("error;Impossible de lire la valeur\n");
+            int myRandom=rand()%100000;
+            printf("error;%d;Impossible de lire la valeur\n",myRandom);
         }
 
         fflush(stdout);
