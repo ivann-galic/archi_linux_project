2537abdf001d17a12cf1339c9d5ee5061be559de
kevin <r_kevin@hotmail.fr>
2020-04-24 11:14:32 +0200

    Story 9 terminé

--- supervision.sh
+++ supervision.sh
@@ -5,18 +5,17 @@ folder_name=$2
 infos_log_file=$3
 errors_log_file=$4
 file_size=$5
+report_file=report.log
 script_execution=true
 
-
 current_path=$(echo $PWD);
-#if $script_execution;
-#then
 
-    #./generation.sh $delay $folder_name $infos_log_file $errors_log_file
-    #while IFS= read -r RAW_LINE; do
+./generation.sh $delay $folder_name $infos_log_file $errors_log_file &
 
-    if [ -e /home/$USER/$folder_name/$infos_log_file ] && [ -e /home/$USER/$folder_name/$errors_log_file ];
-        then
+while [ script_execution ]; do
+ echo "tu entres dans la boucle while $script_execution";
+ if [ -e /home/$USER/$folder_name/$infos_log_file ] && [ -e /home/$USER/$folder_name/$errors_log_file ];
+    then
             # get the size of the logs files:
             size_infos=`du -k /home/$USER/$folder_name/$infos_log_file | cut -f1`
             size_errors=`du -k /home/$USER/$folder_name/$errors_log_file| cut -f1`
@@ -45,18 +44,22 @@ current_path=$(echo $PWD);
             if [ $size_infos -gt $file_size ] || [ $size_errors -gt $file_size ]
             then
                 # kills the generation script, compresses log files :
-                killall -9 generation.sh;
-                cd /home/coding/$folder_name/
-                tar -zcvf "$(date '+%Y-%m-%d').tar.gz" $infos_log_file $errors_log_file
+                killall generation.sh;
+                cd $HOME/$folder_name/
+                number_line_infos=$(wc -l $infos_log_file | cut -d' ' -f1);
+                number_line_errors=$(wc -l $errors_log_file | cut -d' ' -f1);
+                touch report.log
+                echo "$infos_log_file countains $number_line_infos lines." > $report_file
+                echo "$errors_log_file countains $number_line_errors lines." >> $report_file
+                tar -zcvf "$(date '+%Y-%m-%d').tar.gz" $report_file $infos_log_file $errors_log_file
                 # removes the last log files, creates new ones and retart generation script :
-                rm $infos_log_file $errors_log_file
+                rm $infos_log_file $errors_log_file $report_file
                 touch $infos_log_file $errors_log_file
-                
                 cd $current_path
             fi
 
         else 
             echo "Error: Missing log(s) file(s). Please execute generation.sh.";
     fi
-   #done 
-#fi
+sleep 3
+done 
